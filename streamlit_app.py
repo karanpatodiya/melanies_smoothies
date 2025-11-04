a5f8bbe9d2cf2a1425d8a95a1aa9576e02a3d5c2
karanpatodiya <karan.patodiya@credencys.com>
2025-11-04 17:21:00 +0530

    Moving JSON to loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ if ingredients_list:
     ingredients_string=' '
     for each_fruit in ingredients_list:
         ingredients_string+=each_fruit + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -42,6 +44,4 @@ if ingredients_list:
 
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
